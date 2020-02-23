88d6424aa8bcf77172c331b9496b42a99dfaca47
alvinochieng <56371121+alvinochieng@users.noreply.github.com>
2020-02-23 17:21:32 -0500

    did a lot of stuff

--- Resources/hawaii.sql
+++ Resources/hawaii.sql
@@ -1,17 +1,8 @@
-DROP TABLE IF EXISTS station;
-DROP TABLE IF EXISTS measurements;
-
-CREATE TABLE station(
-	station VARCHAR(255) NOT NULL,
-	name VARCHAR(255) NOT NULL,
-	latitude FLOAT NOT NULL,
-	longitude FLOAT NOT NULL,
-	elevation FLOAT NOT NULL
-);
-
 CREATE TABLE measurements(
+	ID SERIAL PRIMARY KEY NOT NULL,
 	station VARCHAR(255) NOT NULL,
 	date VARCHAR(255) NOT NULL,
 	prcp FLOAT,
-	tobs INT
-);
+	tobs INT,
+	FOREIGN KEY(station) REFERENCES station(station)
+); 
