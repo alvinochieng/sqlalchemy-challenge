84a20afbe941307207af4ff6a92bfeaf4b3b654c
alvinochieng <56371121+alvinochieng@users.noreply.github.com>
2020-02-25 16:44:28 -0500

    working on the flask section

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
-postgresStr = ("postgresql://postgress:password@localhost:5432/climate")
+postgresStr = ("postgresql://postgres:password@localhost:5432/climate")
 engine = create_engine(postgresStr)
 
 Base = automap_base()
@@ -32,4 +32,32 @@ def home():
 def precipitation():
     
     session = Session(engine)
+    
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    
+    session.close()
+    
+    result_dict = []
+    
+    for date, precipitation in results:
+        data_dict={}
+        data_dict["prcp"] = precipitation
+        data_dict["date"] = date
+        result_dict.append(data_dict)
+    
+    return jsonify(result_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    
+    session = Session(engine)
+    
+    stations_json = session.query(Station.station, Station.name).all()
+    
+    session.close()
+    
+    return jsonify(stations_json)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
     
